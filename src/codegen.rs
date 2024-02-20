c1c235fdf6c8f2da0735ee7553a89ec8e1225201
enix403 <fog.code000@gmail.com>
2024-02-20 20:06:54 +0500

    Generate fields info

--- src/codegen.rs
+++ src/codegen.rs
@@ -45,12 +45,61 @@ struct TypeScriptGeneratorInner<W> {
     dest: W,
 }
 
+#[derive(Serialize)]
+struct RenderedField<'a> {
+    name: &'a str,
+    info: String,
+    ty: String,
+}
+
+#[derive(Serialize, Clone)]
+enum TsFieldType {
+    Primitve,
+    Array(Box<TsFieldType>),
+    Message,
+}
+
+impl TsFieldType {
+    fn write_ts_field_type(&self, dest: &mut String) {
+        match self {
+            TsFieldType::Primitve => {
+                write!(dest, "{{ kind: TyKindTag.Primitive }}").unwrap();
+            }
+            TsFieldType::Message => {
+                write!(dest, "{{ kind: TyKindTag.Message }}").unwrap();
+            }
+            TsFieldType::Array(ref inner) => {
+                write!(dest, "{{ kind: TyKindTag.Array, of: ");
+                inner.write_ts_field_type(&mut *dest);
+                write!(dest, "}}");
+            }
+        };
+    }
+}
+
+#[derive(Serialize)]
+struct StructContext<'a> {
+    name: &'a str,
+    fields: Vec<RenderedField<'a>>,
+}
+
+impl From<&TyKind> for TsFieldType {
+    fn from(value: &TyKind) -> Self {
+        match value {
+            TyKind::Primitive(..) => TsFieldType::Primitve,
+            TyKind::UserDefined(..) => TsFieldType::Message,
+            TyKind::Array(inner) => TsFieldType::Array(Box::new(inner.as_ref().into())),
+            TyKind::Nullable(inner) => inner.as_ref().into(),
+        }
+    }
+}
+
 impl<W: Write> TypeScriptGeneratorInner<W> {
     pub fn new(dest: W) -> Self {
         Self { dest }
     }
 
-    fn render_type(dest: &mut String, ty: &TyKind) -> std::fmt::Result {
+    fn render_static_type(dest: &mut String, ty: &TyKind) -> std::fmt::Result {
         match ty {
             TyKind::Primitive(prim) => match prim {
                 PrimitiveType::String => write!(dest, "string")?,
@@ -59,23 +108,15 @@ impl<W: Write> TypeScriptGeneratorInner<W> {
             TyKind::UserDefined(ref name) => write!(dest, "{}", name)?,
             TyKind::Array(ref ty) => {
                 write!(dest, "Array<")?;
-                Self::render_type(dest, ty)?;
+                Self::render_static_type(dest, ty)?;
                 write!(dest, ">")?
             }
             TyKind::Nullable(ref ty) => {
                 write!(dest, "(")?;
-                Self::render_type(dest, ty)?;
+                Self::render_static_type(dest, ty)?;
                 write!(dest, ") | null")?
             }
         }
-
-        Ok(())
-    }
-
-    fn render_field(dest: &mut String, field: &StructField) -> std::fmt::Result {
-        write!(dest, "public {}!: ", field.name)?;
-        Self::render_type(dest, &field.datatype)?;
-        write!(dest, ";\n")?;
 
         Ok(())
     }
@@ -84,7 +125,9 @@ impl<W: Write> TypeScriptGeneratorInner<W> {
         const TEMPLATE: &'static str = r#"
 export class {name} extends StructMessage \{
   getFields(): StructField[] \{
-    return [];
+    return [
+{{ for field in fields }}      \{ name: "{field.name}", ty: {field.info} },
+{{ endfor }}    ];
   }
 
   static create(body: FieldsOf<{name}>): {name} \{
@@ -95,33 +138,27 @@ export class {name} extends StructMessage \{
 {{ endfor }}}
 "#;
 
-        #[derive(Serialize)]
-        struct RenderedField<'a> {
-            name: &'a str,
-            ty: String,
-        }
-
-        #[derive(Serialize)]
-        struct Context<'a> {
-            name: &'a str,
-            fields: Vec<RenderedField<'a>>,
-        }
-
         let mut tt = TinyTemplate::new();
         tt.add_template("main", TEMPLATE).unwrap();
         tt.set_default_formatter(&tinytemplate::format_unescaped);
 
-        let context = Context {
+        let context = StructContext {
             name: &struct_.name,
             fields: struct_
                 .fields
                 .iter()
                 .map(|field| RenderedField {
                     name: &field.name,
+                    info: {
+                        let mut info = String::new();
+                        let ts_field: TsFieldType = (&field.datatype).into();
+                        ts_field.write_ts_field_type(&mut info);
+                        info
+                    },
                     ty: {
-                        let mut rendered = String::new();
-                        Self::render_type(&mut rendered, &field.datatype).unwrap();
-                        rendered
+                        let mut ty = String::new();
+                        Self::render_static_type(&mut ty, &field.datatype).unwrap();
+                        ty
                     },
                 })
                 .collect(),
