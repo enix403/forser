d29fd9d02139a5ddfdd9ff2896d94aeae3bec8a6
enix403 <fog.code000@gmail.com>
2024-02-20 18:52:57 +0500

    Start

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
+use std::fmt::Write as _;
 use std::fs::OpenOptions;
 use std::io::{self, Write};
 use std::path::Path;
 use std::write;
 
-use crate::items::{PrimitiveType, Program, TyKind};
+use crate::items::{PrimitiveType, Program, StructDefinition, StructField, TyKind};
 
 pub trait Language {
     fn lang_id(&self) -> &'static str;
@@ -46,7 +47,7 @@ impl<W: Write> TypeScriptGeneratorInner<W> {
         Self { dest }
     }
 
-    fn write_type(dest: &mut W, ty: &TyKind) -> io::Result<()> {
+    fn render_type(dest: &mut String, ty: &TyKind) -> std::fmt::Result {
         match ty {
             TyKind::Primitive(prim) => match prim {
                 PrimitiveType::String => write!(dest, "string")?,
@@ -55,12 +56,12 @@ impl<W: Write> TypeScriptGeneratorInner<W> {
             TyKind::UserDefined(ref name) => write!(dest, "{}", name)?,
             TyKind::Array(ref ty) => {
                 write!(dest, "Array<")?;
-                Self::write_type(dest, ty)?;
+                Self::render_type(dest, ty)?;
                 write!(dest, ">")?
             }
             TyKind::Nullable(ref ty) => {
                 write!(dest, "(")?;
-                Self::write_type(dest, ty)?;
+                Self::render_type(dest, ty)?;
                 write!(dest, ") | null")?
             }
         }
@@ -68,27 +69,130 @@ impl<W: Write> TypeScriptGeneratorInner<W> {
         Ok(())
     }
 
+    fn render_field(dest: &mut String, field: &StructField) -> std::fmt::Result {
+        write!(dest, "public {}!: ", field.name)?;
+        Self::render_type(dest, &field.datatype)?;
+        write!(dest, ";\n")?;
+
+        Ok(())
+    }
+
+    fn write_struct(&mut self, struct_: &StructDefinition) -> io::Result<()> {
+        let mut field_lines = String::new();
+
+        for f in struct_.fields.iter() {
+            Self::render_field(&mut field_lines, f);
+        }
+
+        // println!("{}", field_lines);
+
+        Ok(())
+    }
+
     fn generate(&mut self, program: &Program) -> io::Result<()> {
-        const INDENT: &'static str = "    ";
+        write!(&mut self.dest, "{}", TS_HEADER)?;
 
         for struct_ in program.structs.iter() {
-            writeln!(&mut self.dest, "type {} = {{", struct_.name.as_str())?;
-
-            for (index, field) in struct_.fields.iter().enumerate() {
-                // The comma and new line
-                if index != 0 {
-                    write!(&mut self.dest, ",\n")?;
-                }
-                // indent
-                write!(&mut self.dest, "{}", INDENT)?;
-                // name: datatype
-                write!(&mut self.dest, "{}: ", field.name)?;
-                Self::write_type(&mut self.dest, &field.datatype)?;
-            }
-
-            writeln!(&mut self.dest, "\n}};\n")?;
+            self.write_struct(struct_);
         }
 
         Ok(())
     }
+
+    // fn generate(&mut self, program: &Program) -> io::Result<()> {
+    //     const INDENT: &'static str = "    ";
+
+    //     for struct_ in program.structs.iter() {
+    //         writeln!(&mut self.dest, "type {} = {{", struct_.name.as_str())?;
+
+    //         for (index, field) in struct_.fields.iter().enumerate() {
+    //             // The comma and new line
+    //             if index != 0 {
+    //                 write!(&mut self.dest, ",\n")?;
+    //             }
+
+    //             // indent
+    //             write!(&mut self.dest, "{}", INDENT)?;
+    //             // name: datatype
+    //             write!(&mut self.dest, "{}: ", field.name)?;
+    //             Self::write_type(&mut self.dest, &field.datatype)?;
+    //         }
+
+    //         writeln!(&mut self.dest, "\n}};\n")?;
+    //     }
+
+    //     Ok(())
+    // }
 }
+
+const TS_HEADER: &'static str = r"#
+const enum TyKindTag {
+  Primitive,
+  Message,
+  Array,
+}
+
+type TyKind =
+  | { kind: TyKindTag.Primitive }
+  | { kind: TyKindTag.Message }
+  | { kind: TyKindTag.Array; of: TyKind };
+
+type StructField = {
+  name: string;
+  ty: TyKind;
+};
+
+function valueToPlainObject(value: any, ty: TyKind) {
+  if (value === null) {
+    return null;
+  }
+  //
+  else if (ty.kind === TyKindTag.Primitive) {
+    return value;
+  }
+  //
+  else if (ty.kind === TyKindTag.Message) {
+    return (value as Message).toPlainObject();
+  }
+  //
+  else if (ty.kind === TyKindTag.Array) {
+    return (value as any[]).map((val) => valueToPlainObject(val, ty.of));
+  } else {
+    throw new Error('Invalid value/ty');
+  }
+}
+
+abstract class Message {
+  abstract toPlainObject(): object;
+}
+
+abstract class StructMessage extends Message {
+  protected abstract getFields(): StructField[];
+  toPlainObject(): object {
+    let result: Record<string, any> = {};
+
+    let fields = this.getFields();
+    for (let i = 0; i < fields.length; ++i) {
+      const { name, ty } = fields[i];
+      const value = this[name];
+
+      result[name] = valueToPlainObject(value, ty);
+    }
+
+    return result;
+  }
+}
+
+namespace forser {
+  export function packMessage<M extends Message>(message: M) {
+    return JSON.stringify((message as Message).toPlainObject());
+  }
+}
+
+type FieldsOf<T> = Pick<
+  T,
+  {
+    [K in keyof T]: T[K] extends Function ? never : K;
+  }[keyof T]
+>;
+#";
